45c6ab8a2b1d76d9d5503a47a65a3b85fbbab5fc
Gustavo Henrique Pinto <ghp2201@gmail.com>
2022-01-29 17:07:39 -0300

    Add GET/id PUT and DELETE endpoints

--- src/controller/book.go
+++ src/controller/book.go
@@ -1,11 +1,14 @@
 package controller
 
 import (
+	"database/sql"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"norest/src/repository"
+	"strconv"
 )
 
 func GetAllBooks(rw http.ResponseWriter, r *http.Request) {
@@ -41,6 +44,95 @@ func CreateBook(rw http.ResponseWriter, r *http.Request) {
 
 	repository.AddBook(&newBook)
 
+	rw.Header().Set("Content-Type", "application/json")
 	rw.WriteHeader(http.StatusCreated)
 	fmt.Fprint(rw, string(requestBody))
+}
+
+func GetBookById(rw http.ResponseWriter, r *http.Request, bookId string) {
+	book, err := repository.GetBookById(bookId)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			rw.WriteHeader(http.StatusNotFound)
+			fmt.Fprintf(rw, "Failed to find book with id: %s", bookId)
+			return
+		}
+
+		rw.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(rw, "Failed to get book, error %+v", err)
+		return
+	}
+
+	bookJson, err := json.Marshal(book)
+
+	if err != nil {
+		rw.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(rw, "Failed to marshal %v as JSON, got error %+v", book, err)
+		return
+	}
+
+	rw.Header().Set("Content-Type", "application/json")
+	rw.WriteHeader(http.StatusOK)
+	fmt.Fprint(rw, string(bookJson))
+}
+
+func DeleteBook(rw http.ResponseWriter, r *http.Request, bookId string) {
+	err := repository.DeleteBook(bookId)
+
+	if err != nil {
+		rw.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(rw, "Failed to delete book, error %+v", err)
+		return
+	}
+
+	rw.WriteHeader(http.StatusNoContent)
+	fmt.Fprint(rw, "")
+}
+
+func UpdateBook(rw http.ResponseWriter, r *http.Request, bookId string) {
+	requestBody, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		rw.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(rw, "Failed to parse request body, go error %+v", err)
+		return
+	}
+
+	var newBook repository.Book
+	err = json.Unmarshal(requestBody, &newBook)
+
+	if err != nil {
+		rw.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(rw, "Failed to unmarshal json, got error %+v", err)
+		return
+	}
+
+	err = repository.UpdateBook(bookId, &newBook)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			rw.WriteHeader(http.StatusNotFound)
+			fmt.Fprintf(rw, "Failed to find book with id: %s", bookId)
+			return
+		}
+
+		rw.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(rw, "Failed to update book, error %+v", err)
+		return
+	}
+
+	bookIdInt, _ := strconv.Atoi(bookId)
+	newBook.Id = bookIdInt
+	bookJson, err := json.Marshal(newBook)
+
+	if err != nil {
+		rw.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(rw, "Failed to marshal %v as JSON, got error %+v", newBook, err)
+		return
+	}
+
+	rw.Header().Set("Content-Type", "application/json")
+	rw.WriteHeader(http.StatusOK)
+	fmt.Fprint(rw, string(bookJson))
 }
--- src/main.go
+++ src/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"norest/src/controller"
+	"strings"
 )
 
 func main() {
@@ -12,6 +13,20 @@ func main() {
 			controller.GetAllBooks(rw, r)
 		case http.MethodPost:
 			controller.CreateBook(rw, r)
+		}
+	})
+	http.HandleFunc("/api/books/", func(rw http.ResponseWriter, r *http.Request) {
+		// Any pattern ended with "/" will match all subppaterns, so when usign the net/http as a
+		// router we can extract the subpattern as a value
+		id := strings.TrimPrefix(r.URL.Path, "/api/books/")
+
+		switch r.Method {
+		case http.MethodGet:
+			controller.GetBookById(rw, r, id)
+		case http.MethodDelete:
+			controller.DeleteBook(rw, r, id)
+		case http.MethodPut:
+			controller.UpdateBook(rw, r, id)
 		}
 	})
 
